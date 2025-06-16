a35b2be26cfd70bf7fccbeae587b1160a5f1e4b7
minhhieu899 <phungminhhieu899@gmail.com>
2025-06-16 20:32:44 +0700

    first

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Course = require('../models/Course');
+const users = require('../models/User'); // Giả sử bạn có một file users.js chứa dữ liệu người dùng
+const { useResolvedPath } = require('react-router-dom');
+const User = require('../models/User');
 
 // GET /api/courses
 router.get('/courses', async (req, res) => {
@@ -11,5 +14,14 @@ router.get('/courses', async (req, res) => {
     res.status(500).json({ error: 'Lỗi server khi lấy danh sách khóa học' });
   }
 });
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find().lean(); // dùng .lean() để trả về object thường
+    res.json(users); // trả JSON cho API
+  } catch (err) {
+    res.status(500).json({ error: 'Lỗi server khi lấy danh sách người dùng' });
+  }
+})
 
 module.exports = router;
